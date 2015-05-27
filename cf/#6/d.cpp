a857c5a5def037b927b3d73d390b9e933d1fb7a7
yajun <yajunwf@gmail.com>
2015-05-27 18:52:28 +0800

    fix cf#6d

--- cf/#6/d.cpp
+++ cf/#6/d.cpp
@@ -1,39 +1,56 @@
-# include <queue>
 # include <algorithm>
 # include <stdio.h>
-# include <math.h>
 
 const int maxn = 15;
+const int maxh = 20;
+
 int n, a, b;
 int h[maxn];
-int q[maxn];
+int g[maxn][maxh][maxh];
 
-int ans = 0;
+const int INF = 233;
+
+int pre[maxh][maxh][maxh];
 
 int main()
 {
     // freopen("in.txt", "r", stdin);
-
     scanf("%d%d%d", &n, &a, &b);
-    for (int i = 0; i < n; ++i) scanf("%d", &h[i]);
-
-    for (int i = 1; i < n-1; ++i) {
-        if (h[i-1] < 0 && i+1<n-1) continue;
-        int t = (int)ceil((h[i-1]+1.0)/b);
-        if (i+1==n-1) t = std::max(t, std::max((int)ceil((h[i]+1.0)/a), (int)ceil((h[i+1]+1.0)/b)));
-        h[i-1] -= t*b;
-        h[i] -= t * a;
-        h[i+1] -= t*b;
-        ans += t;
-        q[i] = t;
-    }
-    printf("%d\n", ans);
-    for (int i = 1; i < n-1; ++i) {
-        for (int j = 0; j < q[i]; ++j) {
-            printf("%d ", i+1);
+    for (int i = 1; i <= n; ++i) {scanf("%d", &h[i]); ++h[i];}
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < maxh; ++j)  {
+            for (int k = 0; k < maxh; ++k) {
+                g[i][j][k] = INF;
+            }
         }
+    }
+    g[1][h[1]][h[2]] = 0;
+    for (int i = 2; i < n; ++i) {
+        for (int j = 0; j <= h[i-1]; ++j) {
+            for (int k = 0; k <= h[i]; ++k) {
+                if (g[i-1][j][k] >= INF) continue;
+                int lb = (j+b-1) / b;
+                int ub = std::max(lb, std::max((k+a-1)/a, (h[i+1]+b-1)/b));
+                for (int l = lb; l <= ub; ++l) {
+                    int x = std::max(0, k-l*a);
+                    int y = std::max(0, h[i+1]-l*b);
+                    if (g[i][x][y] > g[i-1][j][k] + l) {
+                        g[i][x][y] = g[i-1][j][k] + l;
+                        pre[i][x][y] = (j<<8)|k;
+                    }
+                }
+            }
+        }
+    }
+    printf("%d\n", g[n-1][0][0]);
+    int x = 0, y = 0, i = n-1;
+    while (i > 1) {
+        int l = pre[i][x][y];
+        int t = g[i][x][y] - g[i-1][l>>8][l&0xff];
+        for (int j = 0; j < t; ++j) printf("%d ", i);
+        x = (l>>8)&0xff, y = (l&0xff);
+        --i;
     }
 
     return 0;
 }
-
