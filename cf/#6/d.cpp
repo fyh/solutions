ba0b5c03754ad90f840469589b6f48a97df56fe3
yajun <yajunwf@gmail.com>
2015-05-27 18:05:44 +0800

    rm empty folders

--- cf/#6/d.cpp
+++ cf/#6/d.cpp
@@ -12,7 +12,7 @@ int ans = 0;
 
 int main()
 {
-     // freopen("in.txt", "r", stdin);
+    // freopen("in.txt", "r", stdin);
 
     scanf("%d%d%d", &n, &a, &b);
     for (int i = 0; i < n; ++i) scanf("%d", &h[i]);
@@ -21,15 +21,16 @@ int main()
         if (h[i-1] < 0 && i+1<n-1) continue;
         int t = (int)ceil((h[i-1]+1.0)/b);
         if (i+1==n-1) t = std::max(t, std::max((int)ceil((h[i]+1.0)/a), (int)ceil((h[i+1]+1.0)/b)));
+        h[i-1] -= t*b;
         h[i] -= t * a;
         h[i+1] -= t*b;
         ans += t;
         q[i] = t;
     }
     printf("%d\n", ans);
-    for (int i = 1; i < n+1; ++i) {
+    for (int i = 1; i < n-1; ++i) {
         for (int j = 0; j < q[i]; ++j) {
-            printf("%d ", std::min(i+1, n));
+            printf("%d ", i+1);
         }
     }
 
