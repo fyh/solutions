ffca9f907ee5d9da83a5e093cfcbcc3bd0b8f0ac
yajun <yajunwf@gmail.com>
2015-06-19 09:14:16 +0800

    fix cf#308d2 c

--- contest/cf#308d2/c.cpp
+++ contest/cf#308d2/c.cpp
@@ -17,7 +17,7 @@ int main()
     bool ok = true;
     for (int i = 0; i < n; ++i) {
         if (a[i] == 0 || a[i] == 1) continue;
-        if (a[i] == w-1) {
+        if (a[i] == w-1 || a[i] == w) {
             ++a[i+1];
             continue;
         }
