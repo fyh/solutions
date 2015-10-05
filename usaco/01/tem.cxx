f8afa6953db778b7d48f7f9a6ba09136c79a866d
yajun <yajunwf@outlook.com>
2015-10-05 19:42:46 +0800

    usaco go

---
+++ usaco/01/tem.cxx
@@ -0,0 +1,15 @@
+/*
+ID: yajunwf1
+LANG: C++11
+TASK:
+*/
+# include <bits/stdc++.h>
+using namespace std;
+# define in(x) freopen(#x".in", "r", stdin)
+# define out(x) freopen(#x".out", "w", stdout)
+
+
+int main()
+{
+    return 0;
+}
